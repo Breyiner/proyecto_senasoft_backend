f784c3045a1bcf8ead8db7f656d61b64e2768790
Breyiner <breyneracosta7@gmail.com>
2025-10-22 09:41:54 -0500

    creacion y configuracion del api

---
+++ app/Helpers/ResponseFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Helpers;
+
+class ResponseFormatter
+{
+  /**
+   * Create a new class instance.
+   */
+  public static function success($message = "Operación exitosa", $status = 200, $data)
+  {
+
+    return response()->json([
+      "success" => true,
+      "code" => $status,
+      "message" => $message,
+      "data" => $data
+    ], $status);
+  }
+
+  public static function error($message, $status, $errors = [])
+  {
+
+    return response()->json([
+      "success" => false,
+      "code" => $status,
+      "message" => $message,
+      "errors" => $errors
+    ], $status);
+  }
+}
