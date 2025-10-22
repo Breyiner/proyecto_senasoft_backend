795d89b2ba2c67966fd8ec88e977074e9ea1f685
Breyiner <breyneracosta7@gmail.com>
2025-10-22 09:56:54 -0500

    se implementa logica para generos

---
+++ app/Http/Requests/Gender/StoreGenderRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Gender;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreGenderRequest extends FormRequest
+{
+/**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:5|max:20',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'El :attribute es obligatorio.',
+            'name.string' => 'El :attribute debe ser en formato de texto.',
+            'name.min' => 'El :attribute debe tener al menos :min caracteres.',
+            'name.max' => 'El :attribute no debe tener más de :max caracteres.',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'name' => 'nombre',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\Http\Controllers\API\Gender\GenderController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
   return User::all();
+});
+
+Route::prefix('genders')->group(function () {
+  Route::get('/', [GenderController::class, 'index']);
+
+  Route::get('/{gender_id}', [GenderController::class, 'show']);
+
+  Route::post('/', [GenderController::class, 'store']);
+
+  Route::put('/{gender_id}', [GenderController::class, 'update']);
+
+  Route::patch('/{gender_id}', [GenderController::class, 'partialUpdate']);
+
+  Route::delete('/{gender_id}', [GenderController::class, 'destroy']);
+  
 });
